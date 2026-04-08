1fde5fca4a7b16ac1dba6a4559294b90586ba469
koval.alexandra <koval.sashka07@gmail.com>
2026-04-08 14:40:35 +0700

    Update 9.py

--- 9.py
+++ 9.py
@@ -5,7 +5,7 @@ while line != "":
     parts = line.split()
     for i in parts:
         i = i.lower()
-        if i [-1].isalpha() == 0:
+        if i[-1].isalpha() == 0:
             i = i[:-1]
         if i not in words:
             words.append(i)
@@ -19,4 +19,3 @@ while words != []:
     max_index = counts.index(max(counts))
     print(words.pop(max_index))
     counts.pop(max_index)
-
